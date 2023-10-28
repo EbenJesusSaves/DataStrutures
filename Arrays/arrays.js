27976c658f4adf4f18c155b3a4f5807b73ad4b9d
Ebenezer Agyekum <agyekumebenezer2001@gmail.com>
2023-10-28 09:44:26 +0000

    array Write ups

---
+++ Arrays/arrays.js
@@ -0,0 +1,8 @@
+//there two types of array
+// statics and dynamic arrays
+// the static arrays are have limited space
+// the dynamic array have room for more elements
+//arrays are best for finding item at their indexes which mostly have O(1)
+
+
+
